bc6e49e9fcbc9923d3bd1023687fb8a7adb7854f
m4fc <mateovasquezus@gmail.com>
2025-09-24 16:18:11 +0000

    created data types lesson yayayayay

--- lesson00_print.py
+++ lesson00_print.py
@@ -1,1 +1,28 @@
 print("Sum: ", 4 + 5)
+x= "herro"
+print("herro:", 6)
+# DATA TYPES
+
+# 1. STRINGS
+print("in reality chegma, however is infact not smode. \nif you look into the situation the fact is \n smode is chegma not that chegma is smode ")
+print("Name: \tBilly \nAge: \t14 \nGrade: \t9")
+# INTEGERS
+print(5 + 5 - 69)
+print( 41 - 67)
+# FLOATS
+print( 4.0) # BC DECIMAL STILL A FLOAT
+print(2.0+4) # STILL A FLOAT EVEN AFTER ADDITION TO NOT A FLOAT :)
+# BOOLEAN
+# ALWAYS A TRUE OR FALSE STATEMENT
+print(True)
+print(False) # LOOKS LIKE A STRING BUT IS NOT IT A BOOLEAN
+print (True+ True) # TRUE IS ONE FALSE IS 0
+print ( True + False)
+print(1==2) # == ASKING IF IT IS TRUE
+print(1==1)
+print (1!=2) # != is ASKING IS IT IS NOT TRUE
+print ("hello"=="hello")
+print (5 + 5 == 11)
+print(type("to see this message, wrap me in a print statement"))
+print(type(99+67))
+print(type(77==67))
