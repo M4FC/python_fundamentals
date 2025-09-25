6c06b14b498e526d9574a2d94145c36447e4babd
m4fc <mateovasquezus@gmail.com>
2025-09-25 17:36:14 +0000

    variables thing

---
+++ lesson 02_variables.py
@@ -0,0 +1,49 @@
+#CONCEPTS VARIABLES
+password = "r8mnpahe"
+email= "mrsmode@wwwsmeg.com"
+print("password:",password) 
+temperature= 72.5
+print(type(temperature))
+enabled = False
+is_complete= True
+print(is_complete)
+print(enabled)
+enabled= True
+print(enabled)
+# variable name must be meaningful
+x=3.14
+y=7
+print(x+y)
+count=10
+count_down=count-1
+print(count_down)
+count=count_down
+print(count)
+print(count_down)
+count=(count_down)
+print(count)
+
+# challenges
+#CHALLENGE 
+Number= 34
+
+ # challenge 2 update variables
+
+x=4
+y="hello"
+temp=x
+x=y
+y=temp
+print(y)
+
+is_raining= False
+is_raining=True
+print("Is it raining?",is_raining)
+
+x=4
+y="b"
+umm=x
+x=y
+y=umm
+print(y)
+print(x)
