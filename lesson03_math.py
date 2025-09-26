2d75985e0d3bf74f74b056a4375113445b3b47a4
m4fc <mateovasquezus@gmail.com>
2025-09-26 18:00:37 +0000

    basic math operations yayayayayay

---
+++ lesson03_math.py
@@ -0,0 +1,39 @@
+#KEY CONCEPTS : math operators: +, -,*,/,//,%,**
+add = 6+9 
+print("Sum: ",add)
+
+subtract= 42-1
+print("difference: ",subtract)
+
+multiply = 6*7
+print("product: ",multiply )
+
+float_divide = 5/2
+print("Quotient: ",float_divide)
+
+floor_division= 5//2
+print("Quotient:", floor_division)
+
+modulus = 7 % 2
+print("Modulus:", modulus)
+
+exponent = 8 ** 2
+print("Exponent:", exponent)
+
+print(2+2 == 10-6)
+print(2+2 != 10-6)
+
+result1= (2+3)*4
+print("RESULT 1:", result1)
+
+result2=2**3*4
+print("RESULT 2:", result2)
+
+result3= 20/5*2
+print("RESULT 3:" , result3)
+
+result4= 10 - 2 + 3
+print("RESULT 4:", result4)
+
+result5= 5+2**3*(4-1)
+print("RESULT5:", result5)
