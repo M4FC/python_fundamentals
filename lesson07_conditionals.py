2fc82f187373254c7ba1de046be526297e626806
m4fc <mateovasquezus@gmail.com>
2025-10-14 16:26:59 +0000

    did conditionals

---
+++ lesson07_conditionals.py
@@ -0,0 +1,56 @@
+#conditionals
+print(4!=2)
+#if
+num=12
+if num==12:
+   print("your number is 19")
+
+num2=12
+print(num2<=10)
+if num2<=19:
+ print("chemga")
+ 
+ temperture=59
+ if temperture > 80:
+   print("its hot")
+ elif temperture >= 60:
+    print("its nice")
+ else:
+   print("its cold")
+x=20
+y=20
+if x==y:
+  print("x is equal to y")
+elif x>y:
+  print("x is greater than y")
+elif x<y:
+  print("x is less than y")
+else:
+  print("error")
+
+
+age= 17
+has_permission=False
+if age>= 17 and has_permission:
+  print("you can drive")
+else:
+  print("u cant drive")
+  # when using and statement both have to be true
+
+  # or 
+
+  day="tuesday"
+
+  if day=="saturday" or day=="sunday":
+    print("it's the weekend")
+  elif day=="monday" or day=="tuesday":
+    print("its the start of the weekday ")
+  else:
+   print("its the weekday")
+
+  if day is not "monday":
+    print("its not monday ")
+    
+    
+
+
