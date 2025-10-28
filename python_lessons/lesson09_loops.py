72d07df7143ffff20fd4d736218f67c83140fffd
m4fc <mateovasquezus@gmail.com>
2025-10-28 17:49:53 +0000

    while loops

--- python_lessons/lesson09_loops.py
+++ python_lessons/lesson09_loops.py
@@ -19,4 +19,34 @@ for i in range(2,11,2):
    print("counting evens", i)
 
 
+count=1
+while count < 5: 
+    print(f"Loopin'. We are on loop # {count}.")
+    count += 1
+    time.sleep(0.5)
+
+print("We have escaped the loop!")
+
+user_input = ""
+
+while user_input != "exit":
+    user_input = input("Type 'exit' to escape:")
+
+count = 60
+increment = 1
+
+while count > 0:
+    count -= increment
+    increment += 1
+        
+    if count < 0:
+        break
+
+    print(count)
+    
+    
+  
+  
+   
+
 
